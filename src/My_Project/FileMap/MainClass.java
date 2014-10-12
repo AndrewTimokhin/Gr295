a0cccb9caba587bd44d2983aa8f7cb33480b6f1f
AndrewTimokhin <andrej.timohin@frtk.ru>
2014-10-12 22:55:18 +0400

    www

--- src/My_Project/FileMap/MainClass.java
+++ src/My_Project/FileMap/MainClass.java
@@ -5,21 +5,21 @@ import java.util.*;
 
 public class MainClass {
 
-	public static void main(String[] args) {
-		Reader rd = new Reader(System.getProperty("db.file").toString());
-		HashMap<String, String> coll = new HashMap<String, String>();
-		coll = rd.read();
-		Set<String> show = coll.keySet();
-		ModeWork mw = new ModeWork(coll);
-		if (args.length == 0)
-			mw.usermode();
-		else {
-			mw.consol(args);
-		}
-		Functional fn = new Functional(coll);
-		fn.list();
-		Collection<String> value = coll.values();
-		Write wr = new Write(args[0]);
-		wr.write(coll);
-	}
+    public static void main(String[] args) {
+        Reader rd = new Reader(System.getProperty("db.file").toString());
+        HashMap<String, String> coll = new HashMap<String, String>();
+        coll = rd.read();
+        Set<String> show = coll.keySet();
+        ModeWork mw = new ModeWork(coll);
+        if (args.length == 0) {
+            mw.usermode();
+        } else {
+            mw.consol(args);
+        }
+        Functional fn = new Functional(coll);
+        fn.list();
+        Collection<String> value = coll.values();
+        Write wr = new Write(args[0]);
+        wr.write(coll);
+    }
 }
--- src/My_Project/FileMap/Write.java
+++ src/My_Project/FileMap/Write.java
@@ -4,30 +4,30 @@ import java.util.*;
 import java.io.*;
 
 public class Write {
-	private String filepath;
-	private HashMap<String, String> map;
+    private String filepath;
+    private HashMap<String, String> map;
 
-	public Write(String path) {
-		filepath = path;
-	}
+    public Write(String path) {
+        filepath = path;
+    }
 
-	public void write(HashMap<String, String> locmap) {
-		map = locmap;
-		try (DataOutputStream out = new DataOutputStream(new FileOutputStream(
-				filepath))) {
-			Set<String> st = map.keySet();
-			for (String time : st) {
-				out.writeInt(time.length());
-				out.writeChars(time);
-				out.writeInt(map.get(time).length());
-				out.writeChars(map.get(time));
+    public void write(HashMap<String, String> locmap) {
+        map = locmap;
+        try (DataOutputStream out = new DataOutputStream(new FileOutputStream(
+                filepath))) {
+            Set<String> st = map.keySet();
+            for (String time : st) {
+                out.writeInt(time.length());
+                out.writeChars(time);
+                out.writeInt(map.get(time).length());
+                out.writeChars(map.get(time));
 
-			}
+            }
 
-		} catch (FileNotFoundException e) {
-			System.out.println("Not Found " + e.toString());
-		} catch (IOException e) {
-			System.out.println("IOException " + e.toString());
-		}
-	}
+        } catch (FileNotFoundException e) {
+            System.out.println("Not Found " + e.toString());
+        } catch (IOException e) {
+            System.out.println("IOException " + e.toString());
+        }
+    }
 }
