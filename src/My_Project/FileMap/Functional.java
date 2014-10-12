0f2fd6b801832d4e618a753f36d871113e23b25c
AndrewTimokhin <andrej.timohin@frtk.ru>
2014-10-12 22:51:41 +0400

    ууу

--- src/My_Project/FileMap/Functional.java
+++ src/My_Project/FileMap/Functional.java
@@ -3,49 +3,49 @@ package ru.fizteh.fivt.students.AndrewTimokhin.FileMap;
 import java.util.*;
 
 class Functional {
-	private HashMap<String, String> map;
+    private HashMap<String, String> map;
 
-	public Functional(HashMap<String, String> mapor) {
+    public Functional(HashMap<String, String> mapor) {
 
-		map = mapor;
-	}
+        map = mapor;
+    }
 
-	void put(String key, String value) {
-		if (map.containsKey(key)) {
-			System.out.println("overwrite");
-			System.out.println(map.get(key));
-		} else {
-			System.out.println("new");
-		}
-		map.put(key, value);
-	}
+    void put(String key, String value) {
+        if (map.containsKey(key)) {
+            System.out.println("overwrite");
+            System.out.println(map.get(key));
+        } else {
+            System.out.println("new");
+        }
+        map.put(key, value);
+    }
 
-	void get(String key) {
-		if (map.containsKey(key)) {
-			System.out.println("found");
-			System.out.println(map.get(key));
-		} else {
-			System.out.println("not found");
-		}
-	}
+    void get(String key) {
+        if (map.containsKey(key)) {
+            System.out.println("found");
+            System.out.println(map.get(key));
+        } else {
+            System.out.println("not found");
+        }
+    }
 
-	void remove(String key) {
-		if (map.containsKey(key)) {
-			map.remove(key);
-		} else {
-			System.out.println("not found");
-		}
+    void remove(String key) {
+        if (map.containsKey(key)) {
+            map.remove(key);
+        } else {
+            System.out.println("not found");
+        }
 
-	}
+    }
 
-	void list() {
-		Set<String> st = map.keySet();
-		if (st.size() == 0) {
-			System.out.println();
-		} else {
-			for (String key : st) {
-				System.out.println(key);
-			}
-		}
-	}
+    void list() {
+        Set<String> st = map.keySet();
+        if (st.size() == 0) {
+            System.out.println();
+        } else {
+            for (String key : st) {
+                System.out.println(key);
+            }
+        }
+    }
 }
