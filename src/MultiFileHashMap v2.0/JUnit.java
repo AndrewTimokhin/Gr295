b46021280cd2cfbb7fa8273ca8516126a3ca7382
AndrewTimokhin <andrej.timohin@frtk.ru>
2014-12-23 02:26:13 +0300

    BETTER

---
+++ src/MultiFileHashMap v2.0/JUnit.java
@@ -0,0 +1,25 @@
+package ru;
+
+import java.io.*;
+import java.util.*;
+
+public class JUnit {
+	public static void main(String[] args) throws NullPointerException,
+			IOException, IllegalAccessException {
+		String path = System.getProperty("fizteh.db.dir").toString();
+		TableWork tb = new TableWork(0, path);
+		Functional fn = new Functional();
+
+		ModeWork mw = new ModeWork();
+
+		mw.usermode(fn, tb);
+		Write wr = new Write();
+		if (tb.t != null)
+			for (int i = 0; i < tb.t.length; i++) {
+
+				wr.write(tb.t[i], tb);
+			}
+
+	};
+
+}
