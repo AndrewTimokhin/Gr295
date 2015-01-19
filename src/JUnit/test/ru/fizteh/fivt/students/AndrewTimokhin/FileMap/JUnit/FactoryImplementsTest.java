a8ec07bddd1d7cef718c2203d5f57fadbc113152
AndrewTimokhin <AndrewTimokhin@users.noreply.github.com>
2015-01-20 00:01:27 +0300

    Update FactoryImplementsTest.java

--- src/JUnit/test/ru/fizteh/fivt/students/AndrewTimokhin/FileMap/JUnit/FactoryImplementsTest.java
+++ src/JUnit/test/ru/fizteh/fivt/students/AndrewTimokhin/FileMap/JUnit/FactoryImplementsTest.java
@@ -16,26 +16,28 @@ import static org.junit.Assert.*;
  * @author Андрей
  */
 public class FactoryImplementsTest {
- private final Map agregaret = new HashMap(); 
-     
+    private final Map agregaret = new HashMap();
+
     /**
      * Тест работает на проверку генерации исключения IllegalArgumentException
      * при попытке создать провайдера базы данных с пустой корневой директорией.
-     * Также тестируеться создание провайдера базы данных с корректной директорией.
+     * Также тестируеться создание провайдера базы данных с корректной
+     * директорией.
+     * 
      * @throws java.io.IOException
      */
 
-    @Test 
+    @Test
     public void testCreate() throws IOException {
-        TableProviderImplements resultOne = (TableProviderImplements) new FactoryImplements().create("C:\\DataBase");
-        assertEquals("C:\\DataBase",resultOne.dir );}
-    
-    @Test(expected = IllegalArgumentException.class) 
-    public void testCreateCorrect() 
-    {
+        TableProviderImplements resultOne = (TableProviderImplements) new FactoryImplements()
+                .create("C:\\DataBase");
+        assertEquals("C:\\DataBase", resultOne.dir);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateCorrect() {
         String dir = null;
         TableProvider result = new FactoryImplements().create(dir);
     }
-    
-        
+
 }
